6712501b89e5565e4d8e0335053fd81fbb2efd93
Oscar Trigueros <otrigueros28>
2019-09-15 15:28:05 -0700

    db added and page working

--- db.js
+++ db.js
@@ -6,9 +6,53 @@ const client = new Client('postgres://localhost/posts');
 
 client.connect();
 
-const SQL = `
-DROP TABLE IF EXISTS posts
-DROP TABLE IF EXISTS tags
+const ReactId = uuid.v4();
+const ExpressId = uuid.v4();
+const NodeId = uuid.v4();
 
-CREATE TABLE tags
+const SQL = `
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts(
+  id UUID PRIMARY KEY,
+  name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE tags(
+  id int PRIMARY KEY,
+  text VARCHAR(255) UNIQUE NOT NULL,
+  post_id UUID REFERENCES posts(id)
+);
+
+INSERT INTO posts(id, name) VALUES ('${ReactId}', 'React');
+INSERT INTO posts(id, name) VALUES ('${NodeId}', 'Node');
+INSERT INTO posts(id, name) VALUES ('${ExpressId}', 'Express');
+
+INSERT INTO tags(id, text, post_id) VALUES (1, 'FrontEnd', '${ReactId}');
+INSERT INTO tags(id, text, post_id) VALUES (2, 'Cool', '${ReactId}');
+INSERT INTO tags(id, text, post_id) VALUES (3, 'Using now', '${ExpressId}');
+INSERT INTO tags(id, text, post_id) VALUES (4, 'A lot of info', '${NodeId}');
+INSERT INTO tags(id, text, post_id) VALUES (5, 'Helpful', '${ExpressId}');
+
 `
+
+const syncAndSeed = async ()=> {
+  await client.query(SQL);
+};
+
+const findAllPosts = async () => {
+  const response = await client.query('SELECT * FROM posts');
+  return response.rows;
+};
+
+const findAllTags = async () => {
+  const response = await client.query('SELECT * FROM tags');
+  return response.rows
+};
+
+module.exports = {
+  syncAndSeed,
+  findAllPosts,
+  findAllTags
+}
